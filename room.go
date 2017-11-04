3f5569d74ae2048a70dd960c5cb374f6b15fffe2
Jonathan Schneider <jschneider@redventures.com>
2017-11-04 12:07:08 -0400

    added run method to room struct. run manages clients joining and leaving rooms, and sending client messages

--- room.go
+++ room.go
@@ -11,3 +11,29 @@ type room struct {
 	// clients holds alll current clients in this room
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// joining
+			r.clients[client] = true
+		case client := <-r.leave:
+			// leaving
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// forward message to all clients
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// send the message
+				default:
+					// failed to send
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
