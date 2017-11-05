ef941135db62e92876df91c250d31afa859cb0db
Jonathan Schneider <jschneider@redventures.com>
2017-11-05 15:35:09 -0500

    added tracer interface implementation to the room struct... then added tracing to the different cases.

--- room.go
+++ room.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/jongschneider/trace"
 )
 
 type room struct {
@@ -17,6 +18,9 @@ type room struct {
 	leave chan *client
 	// clients holds all current clients in this room
 	clients map[*client]bool
+	// tracer will receive trace information of activity
+	// in the room
+	tracer trace.Tracer
 }
 
 func (r *room) run() {
@@ -25,20 +29,24 @@ func (r *room) run() {
 		case client := <-r.join:
 			// joining
 			r.clients[client] = true
+			r.tracer.Trace("New client joined")
 		case client := <-r.leave:
 			// leaving
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("Client left")
 		case msg := <-r.forward:
 			// forward message to all clients
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// send the message
+					r.tracer.Trace(" -- sent client")
 				default:
 					// failed to send
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- failed to send, cleaned up client")
 				}
 			}
 		}
